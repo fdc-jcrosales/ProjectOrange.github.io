5b9f8f036b51a121fd7a096e6c4ed896df3e66fa
FDC-Mark <79072466+FDC-Mark@users.noreply.github.com>
2021-06-04 17:01:42 +0800

    mod design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 (function(){
-    console.log("test");
+    function scrollFunction() {
+        var $navbar = document.getElementById("navbar");
+        var $logo = document.getElementById("logo");
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            $navbar.style.padding = "0rem 1rem";
+            $logo.style.height = "45px";
+        } else {
+            $navbar.style.padding = ".5rem 1rem";
+            $logo.style.height = "50px";
+        }
+    }
+
+    window.onscroll = function() {scrollFunction()};
 })();
